7ab69a22d2ed2c76050619cc81b15dcfd7be0ceb
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-26 12:23:50 +0200

    Add validation tests for Location model
    - Tests that a Location is valid with a name between 3 and 50 characters
    - Tests that a Location is invalid without a name
    - Tests that a Location is invalid with a name less than 3 characters
    - Tests that a Location is invalid with a name more than 50 characters

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    @location = Location.new(name: 'Windhoek')
+  end
+  it 'should be valid' do
+    expect(subject).to be_valid
+  end
+  it 'should be invalid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+  it 'should be invalid with a name less than 3 characters' do
+    subject.name = 'ab'
+    expect(subject).to_not be_valid
+  end
+  it 'should be invalid with a name more than 50 characters' do
+    subject.name = 'a' * 51
+    expect(subject).to_not be_valid
+  end
 end
