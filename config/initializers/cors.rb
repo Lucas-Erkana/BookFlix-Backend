a8a0390c1698a3ab073e1e2ac63b8e2a1449a697
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-28 11:47:15 +0200

    Update cors.rb
    Remove local host

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:3001'
-      resource(
-        '*',
-        headers: :any,
-        expose: ['access-token', 'expiry', 'token-type',"Authorization"],
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      )
-    end
+  allow do
+    origins '*'
+    resource(
+      '*',
+      headers: :any,
+      expose: ['access-token', 'expiry', 'token-type',"Authorization"],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    )
+  end
 end
-  
