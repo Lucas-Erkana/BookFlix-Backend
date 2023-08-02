055a8eff4bcb6a40fb0786a0526e6103fb41c300
Lucas David Erkana <lderkana21x@gmail.com>
2023-08-02 20:47:20 +0200

    Revert "Final Group Capstone - Book a Movie with Bookflix"

--- app/models/reservation.rb
+++
@@ -1,8 +0,0 @@
-class Reservation < ApplicationRecord
-  belongs_to :location
-  belongs_to :user
-  belongs_to :movie
-
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-end
--- app/serializers/user_serializer.rb
+++
@@ -1,7 +0,0 @@
-class UserSerializer
-  include JSONAPI::Serializer
-  attributes :id, :email, :role, :full_name
-  attribute :created_date do |user|
-    user&.created_at&.strftime('%d/%m/%Y')
-  end
-end
